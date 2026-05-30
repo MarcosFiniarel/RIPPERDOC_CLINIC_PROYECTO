f0c144f363843db61cbf6ead195f5e2d63971aad
Marcos <ma.lizanag@duocuc.cl>
2026-05-29 22:45:44 -0400

    Modificacion de Patient
    Modificacion de Model y Repository.
    Añadidura de Service, Controller y Global Handler.
    A la espera de Billetera para generar el trigger del mismo en la creacion de un paciente.

--- RIPPERDOC_CLINIC/cyber-patient-service/src/main/java/com/cps/cyber_patient_service/Model/Paciente.java
+++ RIPPERDOC_CLINIC/cyber-patient-service/src/main/java/com/cps/cyber_patient_service/Model/Paciente.java
@@ -18,12 +18,10 @@ public class Paciente {
 
     @NotBlank(message = "El alias del paciente es obligatorio")
     @Size(max = 50, message = "El alias no puede superar los 50 caracteres")
+    @Column(unique = true)
     private String alias;
 
     @NotNull(message = "El nivel de cyberpsicosis no puede ser nulo")
     @Min(value = 0, message = "El nivel mínimo es 0%")
     private Integer nivelCyberpsicosis = 0;
-
-    @NotBlank(message = "El estado de salud mental es obligatorio")
-    private String estado = "ESTABLE"; // Valores lógicos: "ESTABLE", "CIBERPSICOPATA"
 }
