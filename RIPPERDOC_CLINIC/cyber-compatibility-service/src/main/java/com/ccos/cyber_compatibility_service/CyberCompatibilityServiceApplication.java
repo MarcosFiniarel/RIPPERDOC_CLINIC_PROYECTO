475a22015604b02e03911a20296f82b18f7de2e7
Marcos <ma.lizanag@duocuc.cl>
2026-06-02 15:23:04 -0400

    Añadidura de Compatibility
    Creacion del microservicio compatibility

---
+++ RIPPERDOC_CLINIC/cyber-compatibility-service/src/main/java/com/ccos/cyber_compatibility_service/CyberCompatibilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ccos.cyber_compatibility_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberCompatibilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberCompatibilityServiceApplication.class, args);
+	}
+
+}
---
+++ RIPPERDOC_CLINIC/cyber-compatibility-service/src/test/java/com/ccos/cyber_compatibility_service/CyberCompatibilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccos.cyber_compatibility_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberCompatibilityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
